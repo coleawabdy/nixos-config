f999ba17f233f720bf1186291ae681a09cbfa544
Cole Awabdy <contact@coleawabdy.com>
2022-12-15 06:58:57 +0000

    Add hardware conf

--- hosts/hydrogen.nix
+++ hosts/hydrogen.nix
@@ -1,6 +1,29 @@
-{
+{  
   system = "x86_64-linux";
   host = { config, pkgs, lib, ... }:
   {
- };
+    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usb_hid" "sd_mod" ];
+    boot.initrd.kernelModules = [ "dm-snapshot" ];
+    boot.extraKernelModules = [ ];
+
+    fileSystems."/" = 
+      {
+        device = "/dev/main/root";
+        fsType = "btrfs";
+      };
+
+    fileSystems."/boot" = 
+      {
+        device = "/dev/disk/by-label/BOOT";
+        fsType = "vfat";
+      };
+
+    swapDevices = [];
+
+    networking.useDHCP = lib.mkDefault true;
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  };
 }
