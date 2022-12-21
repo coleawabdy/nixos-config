75fbc2d827b608d82586783deac84c13e7871dab
Cole Awabdy <contact@coleawabdy.com>
2022-12-21 03:47:18 +0000

    Some firefox configuration

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+    
+    nurpkgs.url = github:nix-community/NUR;
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -27,11 +29,13 @@
     let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
-
+        overlays = [ nurpkgs.overlay ];
         config.allowUnfree = true;
       };
     in home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
+
+      
 
       modules = [
         hyprland.homeManagerModules.default
--- users/cole/home.nix
+++ users/cole/home.nix
@@ -48,7 +48,20 @@ in {
 
   programs.firefox = {
     enable = true;
-
+    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      bitwarden
+      ublock-origin
+      localcdn
+    ];
+    profiles.default = {
+      id = 0;
+      name = "Default";
+      isDefault = true;
+      settings = {
+        "extensions.pocket.enabled" = false;
+        "identity.fxaccounts.enabled" = false;  
+      };
+    };
   };
 
 
