3aaa9aad65d7ba4063bdd97cb427c2663a18f53c
Cole Awabdy <contact@coleawabdy.com>
2022-12-22 20:59:13 +0000

    Added nord theme rofi config

--- homes/modules/desktop/desktop.nix
+++ homes/modules/desktop/desktop.nix
@@ -11,7 +11,6 @@
   xdg.configFile = {
     "hypr/hyprland.conf".source = ./hyprland.conf;
     "rofi/config.rasi".source = ./rofi.rasi;
-    "rofi/vars.rasi".source = ./vars.rasi;
     "wallpaper.png".source = ./wallpaper.png;
   };
 }
