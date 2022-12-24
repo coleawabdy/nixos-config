67c62d5caaa7a856c10b722be656ccd353aae130
Cole Awabdy <contact@coleawabdy.com>
2022-12-24 19:30:22 +0000

    Fixes for helium bluetooth

--- hosts/helium.nix
+++ hosts/helium.nix
@@ -11,6 +11,7 @@ let
     ];
     boot.initrd.kernelModules = [ "dm-snapshot" ];
     boot.kernelModules = [ "kvm-intel" ];
+    boot.blacklistedKernelModules = [ "psmouse" ];
     boot.initrd.luks.devices.nixos = {
       device = "/dev/disk/by-label/NIXOS";
       preLVM = true;
@@ -38,7 +39,8 @@ let
 
     powerManagement.cpuFreqGovernor = "powersave";
     hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
-    
+    hardware.enableRedistributableFirmware = true;
+
     boot.kernel.sysctl = {
       "kernel.unprivileged_userns_clone" = 1;
     };
@@ -56,6 +58,8 @@ let
       jack.enable = true;
       wireplumber.enable = true;
     };
+
+    services.fwupd.enable = true;
     
     hardware.opengl.enable = true;
     hardware.pulseaudio.enable = false;
