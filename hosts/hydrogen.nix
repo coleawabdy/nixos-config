211948d24567cb4abab94db71e7973dc11a47e57
Cole Awabdy <contact@coleawabdy.com>
2023-01-14 21:11:52 -0700

    Hydrogen updates

--- hosts/hydrogen.nix
+++ hosts/hydrogen.nix
@@ -27,3 +27,37 @@
 
   };
 }
+
+let
+  config = { config, lib, pkgs, ... }: {
+    boot.initrd.availableKernelModules = [
+      "nvme"
+      "xhci_pci"
+      "usb_storage"
+      "usbhid"
+      "sd_mod"
+    ];
+    boot.initrd.kernelModules = [ "dm-snapshot" ];
+    
+    fileSystems = {
+      "/" = {
+        device = "/dev/main/root";
+        fsType = "btrfs";
+      };
+
+      "/boot" = {
+        device = "/dev/disk/by-label/BOOT";
+        fsType = "vfat";
+      };
+    };
+
+    swapDevices = [
+      { device = "/dev/main/swap"; }
+    ];
+
+  };
+in { inputs }: {
+  system = "x86_64-linux";
+  modules = [
+  ];
+}
