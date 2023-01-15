5de7e9987e15ce4cb005695e35a27d00d6551575
Cole Awabdy <contact@coleawabdy.com>
2023-01-14 21:12:33 -0700

    More hydrogen fixes

--- hosts/hydrogen.nix
+++ hosts/hydrogen.nix
@@ -1,33 +1,3 @@
-{  
-  system = "x86_64-linux";
-  host = { config, pkgs, lib, ... }:
-  {
-    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usb_hid" "sd_mod" ];
-    boot.initrd.kernelModules = [ "dm-snapshot" ];
-    boot.extraKernelModules = [ ];
-
-    fileSystems."/" = 
-      {
-        device = "/dev/main/root";
-        fsType = "btrfs";
-      };
-
-    fileSystems."/boot" = 
-      {
-        device = "/dev/disk/by-label/BOOT";
-        fsType = "vfat";
-      };
-
-    swapDevices = [];
-
-    networking.useDHCP = lib.mkDefault true;
-
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  };
-}
-
 let
   config = { config, lib, pkgs, ... }: {
     boot.initrd.availableKernelModules = [
@@ -59,5 +29,6 @@ let
 in { inputs }: {
   system = "x86_64-linux";
   modules = [
+    config
   ];
 }
