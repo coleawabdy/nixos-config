c1aa624661f9b8aed41402ad95cb616f72abbd19
Cole Awabdy <contact@coleawabdy.com>
2023-01-16 17:45:27 -0700

    Some modifications for hydrogen

--- hosts/hydrogen.nix
+++ hosts/hydrogen.nix
@@ -7,7 +7,13 @@ let
       "usbhid"
       "sd_mod"
     ];
-    boot.initrd.kernelModules = [ "dm-snapshot" ];
+    boot.initrd.kernelModules = [
+			"dm-snapshot"
+			"nvidia"
+			"nvidia_modeset"
+			"nvidia_uvm"
+			"nvidia_drm"
+		];
     boot.initrd.luks.devices.nixos = {
       device = "/dev/disk/by-label/NIXOS";
       preLVM = true;
@@ -34,6 +40,7 @@ let
       rtkit.enable = true;
     };
 
+
     services.pipewire = {
       enable = true;
       alsa.enable = true;
@@ -42,8 +49,28 @@ let
       wireplumber.enable = true;
     };
 
-    hardware.opengl.enable = true;
+		environment.sessionVariables = {
+			LIBVA_DRIVER_NAME = "nvidia";
+			XDG_SESSION_TYPE = "wayland";
+			GBM_BACKEND = "nvidia-drm";
+			__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+			WLR_NO_HARDWARE_CURSORS = "1";
+			EGL_PLATFORM = "wayland";
+			MOZ_DISABLE_RDD_SANDBOX = "1";
+		};
 
+		environment.systemPackages = with pkgs; [
+		];
+
+		services.xserver.videoDrivers = [ "nvidia" ];
+
+		programs.hyprland.nvidiaPatches = true;
+		
+		hardware.opengl.extraPackages = with pkgs; [
+			nvidia-vaapi-driver
+		];
+    hardware.opengl.enable = true;
+		hardware.nvidia.modesetting.enable = true;
     hardware.pulseaudio.enable = false;
 
     services.dbus.enable = true;
@@ -53,6 +80,7 @@ in { inputs }: {
   modules = [
     inputs.hyprland.nixosModules.default
     (import ./modules/desktop.nix)
+		(import ./modules/security.nix)
     config
   ];
 }
